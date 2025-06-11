1f737cb6191f0e6df9a0ef0def621e914ea13ae8
b1g-Dr4goN <kuanmin.bigdragon.56@gmail.com>
2025-06-11 11:38:36 +0700

    minor changes

--- src/components/MyOrthanc/myApi.js
+++ src/components/MyOrthanc/myApi.js
@@ -1,14 +1,6 @@
 import { myOrthancApi } from "./myConfigurations";
 import axios from 'axios';
 
-// const username = 'orthanc';
-// const password = 'orthanc';
-// const auth = 'Basic ' + btoa(username + ':' + password);
-// const headers = {
-//     'Authorization': auth,
-//     'Content-Type': 'application/json',
-// };
-
 export default {
     async getEventQueues() {
         return ((await axios.get(myOrthancApi + "/event-queues")).data);
